a2b55c59330c9e3a8f10844c78d14347f8808e5a
Shane Barton <shanecb@hotmail.ca>
2024-09-10 12:35:52 -0400

    Clean up function/make static for future accessibility

--- Assets/Scripts/MouseWorld.cs
+++ Assets/Scripts/MouseWorld.cs
@@ -2,11 +2,24 @@ using UnityEngine;
 
 public class MouseWorld : MonoBehaviour
 {
+    [SerializeField] private LayerMask mousePlaneLayerMask;
+
+    private static MouseWorld instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
 
     private void Update()
     {
+        transform.position = MouseWorld.GetPosition();
+    }
+
+    public static Vector3 GetPosition()
+    {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        Physics.Raycast(ray, out RaycastHit raycastHit);
-        transform.position = raycastHit.point;
+        Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, instance.mousePlaneLayerMask);
+        return raycastHit.point;
     }
 }
