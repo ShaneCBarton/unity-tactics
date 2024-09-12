033ca678669db892337b9d305d3c44e683d5dc25
Shane Barton <shanecb@hotmail.ca>
2024-09-12 12:43:54 -0400

    Add function to select different units to control

--- Assets/Scripts/UnitManager.cs
+++ Assets/Scripts/UnitManager.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class UnitManager : MonoBehaviour
 {
     [SerializeField] private Unit selectedUnit;
+    [SerializeField] private LayerMask unitLayerMask;
 
     private void Update()
     {
@@ -13,7 +14,23 @@ public class UnitManager : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
+            if (TryHandleUnitSelection()) { return; }
             selectedUnit.Move(MouseWorld.GetPosition());
         }
+    }
+
+    private bool TryHandleUnitSelection()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit raycastHit, float.MaxValue, unitLayerMask))
+        {
+            if (raycastHit.transform.TryGetComponent<Unit>(out Unit unit))
+            {
+                selectedUnit = unit;
+                return true;
+            }
+        }
+
+        return false;
     }
 }
