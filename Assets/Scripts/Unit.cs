1d35c3d3188878b716cfc49c3ef5abd96ee35d64
Shane Barton <shanecb@hotmail.ca>
2024-09-12 10:44:07 -0400

    Enable walk animation during movement

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -3,26 +3,40 @@ using UnityEngine;
 public class Unit : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
+    [SerializeField] private Animator unitAnimator;
 
     private float stoppingDistance = .1f;
     private Vector3 targetPosition;
 
     private void Update()
     {
-        if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
-        {
-            Vector3 moveDirection = (targetPosition - transform.position).normalized;
-            transform.position += moveDirection * Time.deltaTime * moveSpeed;
-        }
+        UnitMovement();
+        MouseInput();
+    }
 
+    private void Move(Vector3 targetPosition)
+    {
+        this.targetPosition = targetPosition;
+    }
+
+    private void MouseInput()
+    {
         if (Input.GetMouseButtonDown(0))
         {
             Move(MouseWorld.GetPosition());
         }
     }
 
-    private void Move(Vector3 targetPosition)
+    private void UnitMovement()
     {
-        this.targetPosition = targetPosition;
+        if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
+        {
+            Vector3 moveDirection = (targetPosition - transform.position).normalized;
+            transform.position += moveDirection * Time.deltaTime * moveSpeed;
+            unitAnimator.SetBool("IsWalking", true);
+        } else
+        {
+            unitAnimator.SetBool("IsWalking", false);
+        }
     }
 }
