c0234e7db4f6cce36b0c32fe326060f2bde28364
Shane Barton <shanecb@hotmail.ca>
2024-09-12 10:59:24 -0400

    Make unit rotate towards move position

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -6,12 +6,13 @@ public class Unit : MonoBehaviour
     [SerializeField] private Animator unitAnimator;
 
     private float stoppingDistance = .1f;
+    private float rotationSpeed = 10f;
     private Vector3 targetPosition;
 
     private void Update()
     {
-        UnitMovement();
-        MouseInput();
+        PollMouseInput();
+        PollUnitMovement();
     }
 
     private void Move(Vector3 targetPosition)
@@ -19,7 +20,7 @@ public class Unit : MonoBehaviour
         this.targetPosition = targetPosition;
     }
 
-    private void MouseInput()
+    private void PollMouseInput()
     {
         if (Input.GetMouseButtonDown(0))
         {
@@ -27,14 +28,17 @@ public class Unit : MonoBehaviour
         }
     }
 
-    private void UnitMovement()
+    private void PollUnitMovement()
     {
         if (Vector3.Distance(transform.position, targetPosition) > stoppingDistance)
         {
             Vector3 moveDirection = (targetPosition - transform.position).normalized;
+            transform.forward = Vector3.Slerp(transform.forward, moveDirection, Time.deltaTime * rotationSpeed);
             transform.position += moveDirection * Time.deltaTime * moveSpeed;
+
             unitAnimator.SetBool("IsWalking", true);
-        } else
+        } 
+        else
         {
             unitAnimator.SetBool("IsWalking", false);
         }
