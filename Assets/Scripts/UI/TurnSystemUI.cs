70bf34ae818887f146212015d5ed575aa482a74d
Shane Barton <shanecb@hotmail.ca>
2024-10-24 13:19:29 -0400

    Add turn system for tracking turns, add UI

---
+++ Assets/Scripts/UI/TurnSystemUI.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TurnSystemUI : MonoBehaviour
+{
+    [SerializeField] private Button endTurnButton;
+    [SerializeField] private TextMeshProUGUI turnNumberText;
+
+    private void Start()
+    {
+        endTurnButton.onClick.AddListener(() =>
+        {
+            TurnSystem.Instance.NextTurn();
+        });
+
+        UpdateTurnText();
+
+        TurnSystem.Instance.OnTurnChanged += TurnSystem_OnTurnChanged;
+    }
+
+    private void TurnSystem_OnTurnChanged(object sender, EventArgs e)
+    {
+        UpdateTurnText();
+    }
+
+    private void UpdateTurnText()
+    {
+        turnNumberText.text = "turn " + TurnSystem.Instance.GetTurnNumber();
+    }
+}
