4c82275a4123a12a739c1358519d8003c81e3c58
Shane Barton <shanecb@hotmail.ca>
2024-10-29 12:40:18 -0400

    Add particle effect to bullet hit.

--- Assets/Scripts/BulletProjectile.cs
+++ Assets/Scripts/BulletProjectile.cs
@@ -6,6 +6,7 @@ public class BulletProjectile : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 200f;
     [SerializeField] private TrailRenderer trailRenderer;
+    [SerializeField] private Transform bulletImpactEffectPrefab;
 
     private Vector3 targetPosition;
 
@@ -19,8 +20,11 @@ public class BulletProjectile : MonoBehaviour
 
         if (distanceBeforeMoving < distanceAfterMoving)
         {
+            transform.position = targetPosition;
+            
             trailRenderer.transform.parent = null;
             Destroy(gameObject);
+            Instantiate(bulletImpactEffectPrefab, targetPosition, Quaternion.identity);
         }
     }
 
