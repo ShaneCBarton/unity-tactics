3411289f4d50f36d46cae867c14fb7d43821f7c8
Shane Barton <shanecb@hotmail.ca>
2024-10-31 11:32:43 -0400

    Add quit functionality to menu, refactored start code

---
+++ Assets/Scripts/Menu/ButtonControl.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ButtonControl : MonoBehaviour
+{
+    [SerializeField] private Button startButton;
+    [SerializeField] private Button quitButton;
+
+    private void Start()
+    {
+        startButton.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene(1);
+        });
+
+        quitButton.onClick.AddListener(() =>
+        {
+            if (Application.isEditor)
+            {
+                EditorApplication.isPlaying = false;
+            }
+            else
+            {
+                Application.Quit();
+            }
+        });
+    }
+}
